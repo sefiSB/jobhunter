58f1179e5c6467aa73bf15c2f8ebe5c677d325cd
sefiSB <balintsch7@gmail.com>
2024-06-10 12:39:02 +0200

    changes to store.js

--- react-bead-jubhunter-starter-kit-main/client/src/App.jsx
+++ react-bead-jubhunter-starter-kit-main/client/src/App.jsx
@@ -1,22 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import JobNav from './Components/JobNav/JobNav'
-import Profile from './Components/Profile/Profile'
-import Home from './Components/Home/Home'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import JobNav from "./Components/JobNav/JobNav";
+import Profile from "./Components/Profile/Profile";
+import Home from "./Components/Home/Home";
 
-import './App.css'
-
+import "./App.css";
+import {
+  changeAuth,
+  useAuthenticateMutation,
+  useGetJobsQuery,
+  useGetUserQuery,
+} from "./store/store";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [authenticate] = useAuthenticateMutation();
+  authenticate({data:{
+    email: "user1@jobhunter.hu",
+    password: "user1",
+    strategy: "local",}
+  }).then((result)=>{
+    console.log(result)
+  }).catch((error)=>{
+    console.log(error)
+  });
 
+  /* if (isLoading) {
+    return <></>;
+  }
+  if (error) {
+    return (
+      <>
+        <p>Szar lehet</p>
+      </>
+    );
+  } */
+
+  
   return (
     <>
-    <JobNav/>
-    <Profile></Profile>
-    </>  
-  )
+      <JobNav />
+      <Home />
+    </>
+  );
 }
 
-export default App
+export default App;
--- react-bead-jubhunter-starter-kit-main/client/src/store/store.js
+++ react-bead-jubhunter-starter-kit-main/client/src/store/store.js
@@ -1,21 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   user: null,
   isEmployer: null,
+  authenticate: null,
 };
 
 const BASE_URL = "http://localhost:3030";
 const jobhunterApiSlice = createApi({
   reducerPath: "jobhunterApi",
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  tagTypes: ["users", "experiences", "jobs", "applicants"],
   endpoints: (builder) => ({
-    tagTypes: ["users", "experiences", "jobs", "applicants"],
     getUser: builder.query({
       query: ({ id }) => `users/${id}`,
-      providesTags: ["user"],
     }),
     getJobs: builder.query({
       query: () => `jobs`,
@@ -34,131 +33,118 @@ const jobhunterApiSlice = createApi({
       providesTags: ["experience"],
     }),
     addJob: builder.mutation({
-      query: ({ data, id }) => {
-        return {
-          url: "users",
-          method: "POST",
-          body: {
-            company: data.company,
-            position: data.position,
-            description: data.description,
-            salaryFrom: data.salaryFrom,
-            salaryTo: data.salaryTo,
-            type: data.type,
-            city: data.city,
-            homeOffice: data.homeOffice,
-          },
-        };
-      },
+      query: ({ data, id }) => ({
+        url: "users",
+        method: "POST",
+        body: {
+          company: data.company,
+          position: data.position,
+          description: data.description,
+          salaryFrom: data.salaryFrom,
+          salaryTo: data.salaryTo,
+          type: data.type,
+          city: data.city,
+          homeOffice: data.homeOffice,
+        },
+      }),
       invalidatesTags: ["job"],
     }),
     applyJob: builder.mutation({
-      query: ({ data, id }) => {
-        return {
-          url: "applicants",
-          method: "POST",
-          body: {
-            jobId: data.jobId,
-          },
-        };
-      },
+      query: ({ data, id }) => ({
+        url: "applicants",
+        method: "POST",
+        body: {
+          jobId: data.jobId,
+        },
+      }),
       invalidatesTags: [""],
     }),
-
+    authenticate: builder.mutation({
+      query: ({ data }) => ({
+        url: "authentication",
+        method: "POST",
+        body: {
+          email: data.email,
+          password: data.password,
+          strategy: data.strategy,
+        },
+      }),
+      invalidatesTags: [""],
+    }),
     addUser: builder.mutation({
-      query: ({ data, id }) => {
-        return {
-          url: "users",
-          method: "POST",
-          body: {
-            email: data.email,
-            password: data.password,
-            fullname: data.fullname,
-            role: data.role,
-          },
-        };
-      },
+      query: ({ data }) => ({
+        url: "users",
+        method: "POST",
+        body: {
+          email: data.email,
+          password: data.password,
+          fullname: data.fullname,
+          role: data.role,
+        },
+      }),
       invalidatesTags: ["user"],
     }),
-
     addExperience: builder.mutation({
-      query: ({ dataList, id }) => {
-        dataList.map((data) => {
-          return {
-            url: "experiences",
-            method: "POST",
-            body: {
-              company: data.company,
-              title: data.title,
-              interval: data.interval,
-            },
-          };
-        });
-      },
+      query: ({ dataList }) => ({
+        url: "experiences",
+        method: "POST",
+        body: dataList.map((data) => ({
+          company: data.company,
+          title: data.title,
+          interval: data.interval,
+        })),
+      }),
       invalidatesTags: ["experience"],
     }),
     modifyExp: builder.mutation({
-      query: ({ data, id }) => {
-        return {
-          url: `experiences/${id}`,
-          method: "PATCH",
-          body: {
-            company: data.company,
-            title: data.title,
-            interval: data.interval,
-          },
-        };
-      },
+      query: ({ data, id }) => ({
+        url: `experiences/${id}`,
+        method: "PATCH",
+        body: {
+          company: data.company,
+          title: data.title,
+          interval: data.interval,
+        },
+      }),
       invalidatesTags: ["experience"],
     }),
     modifyJob: builder.mutation({
-      query: ({ data, id }) => {
-        return {
-          url: `experiences/${id}`,
-          method: "PATCH",
-          body: {
-            company: data.company,
-            position: data.position,
-            description: data.description,
-            salaryFrom: data.salaryFrom,
-            salaryTo: data.salaryTo,
-            type: data.type,
-            city: data.city,
-            homeOffice: data.homeOffice,
-          },
-        };
-      },
+      query: ({ data, id }) => ({
+        url: `experiences/${id}`,
+        method: "PATCH",
+        body: {
+          company: data.company,
+          position: data.position,
+          description: data.description,
+          salaryFrom: data.salaryFrom,
+          salaryTo: data.salaryTo,
+          type: data.type,
+          city: data.city,
+          homeOffice: data.homeOffice,
+        },
+      }),
       invalidatesTags: ["job"],
     }),
     deleteExp: builder.mutation({
-      query: ({ deleteList }) => {
-        deleteList.map((e) => {
-          return {
-            url: `experiences/${e}`,
-            method: "DELETE",
-          };
-        });
-      },
+      query: ({ deleteList }) =>
+        deleteList.map((e) => ({
+          url: `experiences/${e}`,
+          method: "DELETE",
+        })),
       invalidatesTags: ["experience"],
     }),
     deleteJobs: builder.mutation({
-      query: ({ deleteList }) => {
-        deleteList.map((e) => {
-          return {
-            url: `jobs/${e}`,
-            method: "DELETE",
-          };
-        });
-      },
+      query: () => ({
+        url: "experiences",
+        method: "DELETE",
+      }),
       invalidatesTags: ["job"],
     }),
     deleteApply: builder.mutation({
-      query: ({ id }) => {
-        return {
-          url: `applicants?jobId=${id}`,
-          method: "DELETE",
-        };
-      },
+      query: ({ id }) => ({
+        url: `applicants?jobId=${id}`,
+        method: "DELETE",
+      }),
       invalidatesTags: [""],
     }),
   }),
@@ -174,6 +160,9 @@ const stateSlice = createSlice({
     changeIsEmployer(state, action) {
       state.isEmployer = action.payload;
     },
+    changeAuth(state, action) {
+      state.authenticate = action.payload;
+    },
   },
 });
 
@@ -182,26 +171,26 @@ export const store = configureStore({
     jobhunterApi: jobhunterApiSlice.reducer,
     states: stateSlice.reducer,
   },
-
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(jobhunterApiSlice.middleware),
 });
 
-export const { changeUser, changeIsEmployer } = stateSlice.actions;
+export const { changeUser, changeIsEmployer,changeAuth } = stateSlice.actions;
 
 export const {
   useGetUserQuery,
   useGetJobsQuery,
-  useApplicants4JobsQuery,
+  useApplicant4JobsQuery,
   useJobs4ApplicantsQuery,
   useGetExperiencesQuery,
   useAddJobMutation,
   useApplyJobMutation,
+  useAuthenticateMutation,
   useAddUserMutation,
-  useAddExerciseMutation,
+  useAddExperienceMutation,
   useModifyExpMutation,
   useModifyJobMutation,
   useDeleteExpMutation,
   useDeleteJobsMutation,
   useDeleteApplyMutation,
-} = resultsApiSlice;
+} = jobhunterApiSlice;
